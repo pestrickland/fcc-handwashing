45c27eb4ccd57d9d3eba42038476c807891d648a
Paul Strickland <p.e.strickland@gmail.com>
2020-10-03 20:16:05 +0100

    Commit HTML.

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,19 @@
-<h1>Test</h1>
+<script>
+  import HowTo from "./HowTo.svelte";
+  import Timer from "./Timer.svelte";
+</script>
+
+<style>
+  h1, h3 {
+    text-align: center;
+  }
+</style>
+
+<h1>Handwashing App</h1>
+<Timer />
+<HowTo />
+
+<h3>
+<a href="http://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a>
+<a href="http://freesound.org/people/metrostock00/sounds/345086/">Sound Source</a>
+</h3>
