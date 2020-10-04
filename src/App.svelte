56053c546ae007f0a5bb9c38d3f27c0dd05086b6
Paul Strickland <p.e.strickland@gmail.com>
2020-10-04 11:53:12 +0100

    Add sound at end of timer.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
   import HowTo from "./HowTo.svelte";
   import Timer from "./Timer.svelte";
+  let audio;
+  function timerEnds(e) {
+    audio.play();
+  }
 </script>
 
 <style>
@@ -10,10 +14,12 @@
 </style>
 
 <h1>Handwashing App</h1>
-<Timer />
+<Timer on:end={timerEnds}/>
 <HowTo />
 
 <h3>
 <a href="http://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a>
-<a href="http://freesound.org/people/metrostock00/sounds/345086/">Sound Source</a>
+<a href="http://freesound.org/people/metrostock99/sounds/345086/">Sound Source</a>
 </h3>
+
+<audio bind:this={audio} src="sound.mp3"></audio>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,10 +1,12 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import ProgressBar from "./ProgressBar.svelte";
 
   const totalSeconds = 20;
   let secondsLeft = totalSeconds;
   let isRunning = false;
   $: progress = (totalSeconds - secondsLeft) / totalSeconds * 100;
+  const dispatch = createEventDispatcher();
 
   function startTimer() {
     const timer = setInterval(() => {
@@ -14,6 +16,7 @@
         clearInterval(timer);
         isRunning = false;
         secondsLeft = totalSeconds;
+        dispatch("end");
       }
     }, 1000);
   }
@@ -37,8 +40,6 @@
 <div bp="grid">
   <h2 bp="offset-5@md 4@md 12@sm">Seconds left: {secondsLeft}</h2>
 </div>
-
-<h1>{progress}</h1>
 
 <ProgressBar {progress}/>
 
